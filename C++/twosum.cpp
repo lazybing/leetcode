18e8e61595dcbddbf9cd087af6d96d7f42d79b9a
lazybing <libinglimit@gmail.com>
2019-08-15 19:01:30 -0700

    three solutions of the 'two sum problem'

---
+++ C++/twosum.cpp
@@ -0,0 +1,78 @@
+// author: lazybing
+// email:libinglimit@gmail.com
+
+/*
+ * Given an array of integers, return indices of tow numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example: given nums = [2, 7, 11, 15], target = 9
+ * because nums[0] + nums[1] == 9;
+ * return [0, 1]
+ *
+ * Test Case && Boundary Test : 
+ * 1.nums empty  (nums = [], taregt = 9) return vector<int>{}
+ * 2.only has one element (nums = [9], target = 9) return vector<int>{}
+ * 3.no nums[i] + nums[j] == target (nums = [2, 7, 11, 15], target = 100) return vector<int>{}
+ * 4.only the same element twice equals to target 2 * nums[i] == target (nums = [2, 7, 11, 15], taregt = 4) return vector<int>{}
+ */
+class Solution {
+    public:
+        /*
+         * Brute Force Solution
+         *
+         * Time complexity: O(n^2)
+         * Sapce complexity: O(1)
+         */
+        vector<int> twoSum1_ans1(vector<int>& nums, int target) {
+            if (nums.size() <= 1)
+                return vector<int>{};
+
+            for (int i = 0; i < nums.size() - 1; i++)
+                for (int j = i + 1; j < nums.size(); j++)
+                    if (nums[i] + nums[j] == target)
+                        return vector<int>{i, j};
+
+            return vector<int>{};
+        }
+
+        /*
+         * Two-pass Hash Table solution
+         *
+         * Time complexity: O(n)
+         * Sapce complexity: O(n)
+         */
+        vector<int> twoSum_ans2(vector<int>& nums, int target) {
+            unorderde_map<int, int> mapping;
+
+            for (int i = 0; i < nums.size(); i++) {
+                mapping.insert({nums[i], i});
+                //mapping[nums[i]] = i;
+            }
+
+            for (int i = 0; i < nums.size(); i++) {
+                auto j = mapping.find(target - nums[i]);
+                if (j != mapping.end() && j->second != i)
+                    return vector<int>{i, j->second};
+            }
+
+            return vector<int>{};
+        }
+
+        /*
+         * One-pass Hash Table solution
+         *
+         * Time complexity: O(n)
+         * Sapce complexity: O(n)
+         */
+        vector<int> towSum_ans3(vector<int>& nums, int target) {
+            unorderde_map<int, int> mapping;
+            for (int i = 0; i < nums.size(); i++) {
+                mapping[nums[i]] = i;
+                auto j = mapping.find(target - nums[i]);
+                if (j != mapping.end() && j->second != i)
+                    return vector<int> {i, j->second};
+            }
+
+            return vector<int>{};
+        }
+};
