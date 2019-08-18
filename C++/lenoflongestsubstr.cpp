d7f259114248ae0fd6a6ec26c6a246b6ab408d89
lazybing <libinglimit@gmail.com>
2019-08-17 20:22:04 -0700

    longestsubstr description

---
+++ C++/lenoflongestsubstr.cpp
@@ -0,0 +1,21 @@
+// author: lazybing
+// email: libinglimit@gmail.com
+// github: https://github.com/lazybing
+
+/*
+ * Given a string, find the length of the longest substring without repeating characters
+ *
+ * Examples:
+ * Input        Output       Explanation
+ * "abcabcbb"     3          substr:"abc", len is 3
+ * "bbbbb"        1          substr:"b", len is 1
+ * "pwwkew"       3          substr:"wke", len is 3
+ *
+ * Test Case && Boundary Test:
+ * 1. Test If the input string is empty.
+ * 2. Some special cases: all characters in the string is unique/same
+ *
+ * Think more:
+ * 1. how the sliding window algorithm used in this problem
+ * 2. what are the time&space complexity in each solutions
+ */
