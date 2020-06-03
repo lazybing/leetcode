4d82cb26fc00bcdece3c73b61ec5a0f63fdc6c2c
lazybing <libinglimit@gmail.com>
2020-06-02 22:43:20 -0700

    add candy solution

--- C++/0135_candy.cpp
+++ C++/0135_candy.cpp
@@ -18,4 +18,28 @@
  *  Explanation: you can allocate to the first, second and third child with 1, 2, 1 candies respectively.
  *              The third child gets 1 candy because it satisfies the avove two conditions.
  */
+class Solution {
+    public:
+        int candy(vector<int>& ratings) {
+            int count = 0;
+            vector<int> left, right;
 
+            for (int i = 0; i < ratings.size(); i++) {
+                left.push_back(1);
+                right.push_back(1);
+            }
+
+            for (int i = 1; i < ratings.size(); i++)
+                if (ratings[i] > ratings[i - 1])
+                    left[i] = left[i - 1] + 1;
+
+            for (int j = ratings.size() - 1; j > 0; j--)
+                if (ratings[j - 1] > ratings[j])
+                    right[j - 1] = right[j] + 1;
+
+            for (int i = 0; i < ratings.size(); i++)
+                cout += max(right[i], left[i]);
+
+            return count;
+        }
+};
