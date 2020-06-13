430a0a52bd6532213128ee0ff2bfbb2c12ce0379
lazybing <libinglimit@gmail.com>
2020-06-12 21:07:17 -0700

    binary tree inorder traversal stack method

--- C++/0094_binary_tree_inorder_traversal.cpp
+++ C++/0094_binary_tree_inorder_traversal.cpp
@@ -21,6 +21,15 @@
  */
 
 //Recursive solution 
+//The first method to solve this problem is using recursion.
+//This is the classical method and is straightforward.
+//We can define a helper function to implements recursion.
+//
+//Complexity Analysis
+//--Time complexity: O(n). The time complexity is O(n) because
+//  the recursive function is T(n) = 2 * T(n / 2) + 1.
+//--Space complexity: The worst case space required is O(n). and in the 
+//  average case it's O(logn) where n is number of nodes.
 class Solution {
     public:
         void rec(TreeNode *root, vector<int>& res) {
@@ -35,6 +44,31 @@ class Solution {
         vector<int> inorderTraversal(TreeNode *root) {
             vector<int> res;
             rec(root, res);
+
+            return res;
+        }
+};
+
+// Iterating method using stack
+// The strategy is very similiar to the first method, the different is using stack.
+class Solution {
+    public:
+        vector<int> inorderTraversal(TreeNode *root) {
+            vector<int> res;
+            stack<TreeNode *> st;
+            TreeNode *curr = root;
+
+            while (curr != nullptr || !st.empty()) {
+                while (curr != nullptr) {
+                    st.push(curr);
+                    curr = curr->left;
+                }
+
+                curr = st.top();
+                st.pop();
+                res.push_back(curr->val);
+                curr = curr->right;
+            }
 
             return res;
         }
