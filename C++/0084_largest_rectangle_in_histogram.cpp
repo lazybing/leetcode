50c0838bac68117f100696343babe0972ab0dc17
lazybing <libinglimit@gmail.com>
2020-06-13 10:45:49 -0700

    add largest rectangle in histogram solution

--- C++/0084_largest_rectangle_in_histogram.cpp
+++ C++/0084_largest_rectangle_in_histogram.cpp
@@ -28,3 +28,40 @@ class Solution {
         }
 };
 
+class Solution {
+    public:
+        int largestRectangleArea(vector<int> & heights) {
+            if (heights.size() == 0) return 0;
+
+            int *leftLessMin  = new int[heights.size()];
+            int *rightLessMin = new int[heights.size()];
+
+            //search the left side, find the first column less than the curr
+            leftLessMin[0] = -1;
+            for (int i = 1; i < heights.size(); i++) {
+                int left = i - 1;
+                while (left >= 0 && heights[left] >= heights[i])
+                    left = leftLessMin[left];
+                leftLessMin[i] = left;
+            }
+
+            //search the right side, find the first column less than the curr
+            rightLessMin[heights.size() - 1] = heights.size();
+            for (int i = heights.size() - 2; i >= 0; i--) {
+                int right = i + 1;
+                while (right <= heights.size() - 1 && heights[right] >= heights[i])
+                    right = rightLessMin[right];
+                rightLessMin[i] = right;
+            }
+
+            //calculate the max area for rectangle including the column
+            int maxArea = 0;
+            for (int i = 0; i <= heights.size() - 1; i++) {
+                int area = (rightLessMin[i] - leftLessMin[i] - 1) * heights[i];
+                maxArea = max(area, maxArea);
+            }
+
+            return maxArea;
+        }
+};
+
