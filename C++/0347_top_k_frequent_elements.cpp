8bd0c380a875197e4e51ef81af571a2ce6a6bf36
lazybing <libinglimit@gmail.com>
2020-06-16 03:22:29 -0700

    top k frequent element solution

--- C++/0347_top_k_frequent_elements.cpp
+++ C++/0347_top_k_frequent_elements.cpp
@@ -19,6 +19,24 @@
 class Solution {
     public:
         vector<int> topKFrequent(vector<int>& nums, int k) {
-        
+            unordered_map<int, int> map;
+            for (int num: nums)
+                map[num]++;
+
+            vector<int> res;
+            //use the priority queue, like the max-heap, we will keep (size-k) smallest elements in the queue
+            //pair<first, second>: first the frequency, second is number
+            priority_queue<pair<int, int>> pq;
+            for (auto it = map.begin(); it != map.end(); it++) {
+                pq.push(make_pair(it->second, it->first));
+                //once the size bigger than size-k, we will pop the value,
+                //which is the top k frequent element value
+                if (pq.size() > (int)map.size() - k) {
+                    res.push_back(pq.top().second);
+                    pq.pop();
+                }
+            }
+
+            return res;
         }
 };
