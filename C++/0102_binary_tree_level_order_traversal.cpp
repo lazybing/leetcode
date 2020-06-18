4e6c08cd37fcfebad1a9b5997ce65556ce84252e
lazybing <libinglimit@gmail.com>
2020-06-17 22:44:07 -0700

    binary tree level order traversal solution

--- C++/0102_binary_tree_level_order_traversal.cpp
+++ C++/0102_binary_tree_level_order_traversal.cpp
@@ -6,3 +6,27 @@
  * return its level order traversal as:
  * [[3], [9, 20], [15, 7]]
  */
+
+class Solution {
+    private:
+        void levelOrderRec(TreeNode *root, int height, vector<vector<int>> &res) {
+            if (root == nullptr) return;
+
+            if (res.size() == height)
+                res.push_back(vector<int>());
+
+            res[height].push_back(root->val);
+
+            levelOrderRec(root->left,  height + 1, res);
+            levelOrderRec(root->right, height + 1, res);
+        }
+
+    public:
+        vector<vector<int>> levelOrder(TreeNode *root) {
+            vector<vector<int>> res = {};
+
+            levelOrderRec(root, 0, res);
+
+            return res;
+        }
+};
