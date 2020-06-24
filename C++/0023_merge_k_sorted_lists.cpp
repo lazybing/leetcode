4915ed5d4ad81260784fc9af1020bbc35026f27e
lazybing <libinglimit@gmail.com>
2020-06-24 03:38:03 -0700

    merget k sorted list solution

--- C++/0023_merge_k_sorted_lists.cpp
+++ C++/0023_merge_k_sorted_lists.cpp
@@ -63,3 +63,56 @@ class Solution {
             return res->next;
         }
 };
+
+/**
+ * Algorithm
+ *  Compare every k nodes (head of every linked list) and get the node with the smallest value.
+ *  Extend the final sorted linked list with the selected nodes.
+ * Complexity Analysis:
+ *  Time complexity: O(kN) where k is the number of linked lists.
+ *      Almost every selection of node in final linked costs O(k)(k - 1 times comparison).
+ *      There are N nodes in the final linked list.
+ *  Space complexity:
+ *      O(n) Creating a new linked list costs O(n) space.
+ *      O(1) It's not hard to apply in-place method-connect selected nodes instead of creating new nodes to fill the new linked list.
+ */
+
+
+/**
+ * Merge lists one by one
+ * Algorithm
+ * Convert merge k lists problem to merge 2 lists(k - 1) times.
+ * Complexity Analysis
+ *  Time complexity:O(kN) where k is the number of linked lists.
+ *      We can merge two sorted linked list in O(n) time where n is the total number of nodes in two lists.
+ *      Sum up the merge process and we can get O(kN).
+ *  Space complexity:O(1)
+ *      We can merge two sorted linked list in O(1) space.
+ */
+class Solution {
+    private:
+        ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+            if (l1 == nullptr) return l2;
+            if (l2 == nullptr) return l1;
+
+            if (l1->val <= l2->val) {
+                l1->next = mergeTwoLists(l1->next, l2);
+                return l1;
+            } else {
+                l2->next = mergeTwoLists(l1, l2->next);
+                return l2;
+            }
+        }
+    public:
+        ListNode *mergeKLists(vector<ListNode*>&lists) {
+            if (lists.empty()) return nullptr;
+
+            while (lists.size() > 1) {
+                lists.push_back(mergeTwoLists(lists[0], lists[1]));
+                lists.erase(lists.begin());
+                lists.erase(lists.begin());
+            }
+
+            return lists.front();
+        }
+};
