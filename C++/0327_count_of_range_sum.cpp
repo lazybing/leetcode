74b748679901331c03ac212764f92134aeeb20d0
lazybing <libinglimit@gmail.com>
2020-06-25 02:12:47 -0700

    count of range sum solution

--- C++/0327_count_of_range_sum.cpp
+++ C++/0327_count_of_range_sum.cpp
@@ -29,3 +29,64 @@ class Solutiono {
             return res;
         }
 };
+
+//Divide and Conquer Solution
+class Solution {
+    private:
+        int recursive_count(vector<long long>& cums, int i, int , vector<long long>&tmp, int lower, int upper) {
+            if (i == j)
+                return cums[i] >= lower && cums[i] <= upper;
+            if (i > j)
+                return 0;
+
+            int mid = (i + j) / 2;
+            int left = recursive_count(cums, i, mid, tmp, lower, upper);
+            int right = recursive_count(cums, mid + 1, j, tmp, lower, upper);
+
+            //compute the cross-boundary case in O(n)
+            //based on the assumption that both left and right array are sorted
+            int p = i, q = i, k = mid + 1;
+            int cross = 0;
+            for (k = mid + 1; k <= j; k++) {
+                while (p <= mid && cums[k] - cumps[p] > upper)
+                    p++;
+                while (q <= mid && cums[k] -cums[q] >= lower)
+                    q++;
+                cross += q - p;
+            }
+
+            //merge sort in O(n)
+            p = i;
+            q = mid + 1;
+            int o = i;
+
+            while (p <= mid || q <= j) {
+                long long v1 = (p <= mid ? cums[p]: LONG_MAX);
+                long long v2 = (q <= j ? cums[q]: LONG_MAX);
+                if (v1 < v2)
+                    tmp[o++] = cums[p++];
+                else
+                    tmp[o++] = cums[q++];
+            }
+            copy(tmp.begin() + i, tmp.begin() + j + 1, cums.begin() + i);
+            
+            return left + right + cross;
+        }
+
+    public:
+        int countRangeSum(vector<int>& nums, int lower, int upper) {
+            if (nums.size() == 0) return 0;
+
+            //build comulative summation array
+            vector<long long> cums(nums.size(), 0);
+            cums[0] = nums[0];
+
+            for (int i = 1; i < nums.size(); i++)
+                cums[i] = cums[i - 1] + nums[i];
+
+            //run divdide and conquer
+            vector<long long> tmp(nums.size());
+            return recursive_count(cums, 0, nums.size() - 1, tmp, lower, upper);
+        }
+};
+
