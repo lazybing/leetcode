f12e593cfdebe357887e8b6f169d573e273e27a2
lazybing <libinglimit@gmail.com>
2021-06-06 16:20:38 -0700

    copy list random pointer

---
+++ C++/0138_copy_list_random_pointer.cpp
@@ -0,0 +1,9 @@
+/*
+ * A linked list of length n is given such that each node contains an additional random pointer,
+ * which could point to any node in the list, or null.
+ *
+ * Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes,
+ * where each new node has its value set to the value of its corresponding original node. Both
+ * the next and random pointer of the nes nodes should point to new nodes in the copied list such
+ * that the pointers in the
+ */
